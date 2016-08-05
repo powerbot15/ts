f74aceae6bd27a0c38b067be0d560b780eab0482
Oleh <powerbot15@gmail.com>
2016-08-05 18:37:12 +0300

    extended person

--- js/app.ts
+++ js/app.ts
@@ -1,5 +1,7 @@
-import Person = require('./namespaces/person')
+import Person = require('./namespaces/person');
 
-let person = new Person('Test');
+let person = new Person('Test', 22);
 
 console.log(person.getName());
+console.log(person.getAge());
+console.log(person.getEmail());
--- js/namespaces/person.ts
+++ js/namespaces/person.ts
@@ -2,12 +2,34 @@ class Person {
 
     name: string;
 
-    constructor(name : string) {
+    age : number;
+
+    private email : string;
+
+    constructor(name : string, age : number) {
+
         this.name = name;
+
+        this.age = age;
+
+        this.email = this.name + this.age + '@supermail.com';
+
     }
 
-    getName () {
+    getName () : string {
+
         return this.name;
+
+    }
+
+    getAge () : number {
+
+        return this.age;
+
+    }
+
+    getEmail () : string {
+        return this.email;
     }
 }
 
